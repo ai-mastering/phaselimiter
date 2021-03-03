126720f4e57588d7aa72f49dce04bd4af0a95e31
mac2 <mac2@example.com>
2021-03-03 11:11:46 +0900

    fix install script

--- script/install_linux.sh
+++ script/install_linux.sh
@@ -3,7 +3,9 @@
 set -ex
 
 cd $(mktemp -d)
-git clone -b master --depth 1 --single-branch https://github.com/ai-mastering/phaselimiter.git phaselimiter
+curl -OL https://github.com/ai-mastering/phaselimiter/releases/download/v0.1.0/release.tar.xz
+tar -Jxvf release.tar.xz
+#git clone -b master --depth 1 --single-branch https://github.com/ai-mastering/phaselimiter.git phaselimiter
 
 sudo mkdir -p /etc/phaselimiter
 sudo cp -R phaselimiter/* /etc/phaselimiter/
